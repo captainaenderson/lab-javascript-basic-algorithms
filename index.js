9ad15fa62c3637e50b2e8ba60aa4d422a2f023ba
Ann-Kathrin Wohlrab <ann-kathrinwohlrab@imacvonnkathrin.speedport.ip>
2023-02-11 08:56:39 +0100

    done

--- index.js
+++ index.js
@@ -15,8 +15,25 @@ if(hacker1.length > hacker2.length){
 
 // Iteration 3: Loops
 let whiteSpace = '';
-for (i = 0; i< hacker1.length; i++) {
+for (let i = 0; i< hacker1.length; i++) {
     whiteSpace += hacker1[i].toUpperCase() + ' ';
 }
 
 console.log(whiteSpace);
+
+let reversedNavigator = '';
+for (let i = hacker2.length - 1; i >= 0; i--) {
+    reversedNavigator += hacker2[i];
+}
+console.log(reversedNavigator);
+
+
+let a = hacker1;
+let b = hacker2;
+if (a.localeCompare(b) === -1) {
+  console.log("The driver's name goes first.");
+} else if (a.localeCompare(b) === 0) {
+  console.log("What?! You both have the same name?");
+} else {
+  console.log("Yo, the navigator goes first definitely.");
+}
